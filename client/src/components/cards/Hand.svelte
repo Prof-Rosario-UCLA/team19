9ebb59bffe8324bdb39da17c61cbed7bd0a3151c
PrincDani <j.dragoon.c@gmail.com>
2025-05-19 17:50:52 -0700

    implemented a hand of cards component

---
+++ client/src/components/cards/Hand.svelte
@@ -0,0 +1,31 @@
+<!-- Hand.svelte -->
+<script lang="ts">
+  import Card from './Card.svelte';
+  import type { CardType } from '../../lib/types';
+  import { createEventDispatcher } from 'svelte';
+  
+  export let cards: CardType[] = [];
+  export let playable: boolean = false;
+  export let isCurrentPlayer: boolean = false;
+  
+  const dispatch = createEventDispatcher();
+  
+  function handleCardSelect(event) {
+    if (isCurrentPlayer && playable) {
+      dispatch('playCard', event.detail);
+    }
+  }
+</script>
+
+<div class="relative h-40 my-5 min-w-[120px] {isCurrentPlayer ? 'ring-2 ring-yellow-400 rounded-lg' : ''}">
+  {#each cards as card, i}
+    <div class="absolute transition-all duration-200" style="left: {i * 25}px;">
+      <Card 
+        suit={card.suit} 
+        rank={card.rank} 
+        selectable={isCurrentPlayer && playable} 
+        on:cardSelect={handleCardSelect} 
+      />
+    </div>
+  {/each}
+</div>
